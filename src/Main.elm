d265a0b1f3c9ec947bd605511d21c47e516c98a7
Jeremie Jost <jeremiejost@gmail.com>
2021-07-01 10:47:56 +0200

    Use elm-bootstrap

--- src/Main.elm
+++ src/Main.elm
@@ -8,7 +8,14 @@ import Html exposing (Html, text, div)
 import Http
 import Json.Decode exposing (Decoder, list, field, map, map2, string)
 
-baseUrl : String 
+
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Utilities.Flex as Flex
+import Bootstrap.Badge as Badge
+
+baseUrl : String
 baseUrl = "https://now-playing-42nq5.ondigitalocean.app/api"
 
 
@@ -107,9 +114,20 @@ view model =
 
     Success stations ->
       let
-        stationsDivs = stations 
+        stationsDivs = stations
           |> Dict.values
-          |> List.map .name
-          |> List.map (\n -> div [] [ text n ])
+          |> List.map (\station ->
+            ListGroup.li
+              [ ListGroup.attrs [ Flex.block, Flex.justifyBetween, Flex.alignItemsCenter ] ]
+              [ text station.name
+              , Badge.badgePrimary [] [ text "14" ]
+              ]
+          )
       in
-        div [] stationsDivs
+        -- div [] stationsDivs
+        Grid.container []
+            [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
+            , Grid.row []
+                [ Grid.col [] [ ListGroup.ul stationsDivs ] ]
+
+            ]
